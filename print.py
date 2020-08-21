fa0587eef9b4d652088120fdae2c50346ea748e9
marower <32189304+marower@users.noreply.github.com>
2020-08-21 13:36:31 -0700

    Update print.py

--- print.py
+++ print.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
-
 import sys, os, random, getopt, re
 from Adafruit_Thermal import *
 from PIL import Image
 
-printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=5)
+def main():
+    printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=5)
 
 
-printer.printImage('template.png',False)
+    printer.printImage('template.png',False)
 
-printer.feed(2)
+    printer.feed(2)
 
-printer.sleep()      # Tell printer to sleep
-printer.wake()       # Call wake() before printing again, even if reset
-printer.setDefault() # Restore printer to defaults
+    printer.sleep()      # Tell printer to sleep
+    printer.wake()       # Call wake() before printing again, even if reset
+    printer.setDefault() # Restore printer to defaults
+
+if __name__ == '__main__':
+  main()
