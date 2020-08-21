8003c6652a20c0f700094093d295ae95e2c2a18d
marower <32189304+marower@users.noreply.github.com>
2020-08-21 13:29:45 -0700

    Update print.py
    added code to print the template

---
+++ print.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys, os, random, getopt, re
+from Adafruit_Thermal import *
+from PIL import Image
+
+printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=5)
+
+
+printer.printImage('template.png',False)
+
+printer.feed(2)
+
+printer.sleep()      # Tell printer to sleep
+printer.wake()       # Call wake() before printing again, even if reset
+printer.setDefault() # Restore printer to defaults
