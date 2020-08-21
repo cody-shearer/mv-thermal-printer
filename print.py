9e4310b9c949bbe18785301bc370f208a104bb10
marower <32189304+marower@users.noreply.github.com>
2020-08-21 14:22:17 -0700

    added font

--- print.py
+++ print.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 import sys, os, random, getopt, re
 from Adafruit_Thermal import *
-from PIL import Image
+from PIL import Image, ImageFont
+
+fnt = ImageFont.truetype('Magic.ttf', 20)
 
 def main():
     printer = Adafruit_Thermal("/dev/ttyUSB0", 9600, timeout=5)
 
     img = Image.open('template.png')
+    add_name(img, 'text')
     printer.printImage(img, True)
 
     printer.feed(2)
@@ -14,6 +17,10 @@ def main():
     printer.sleep()      # Tell printer to sleep
     printer.wake()       # Call wake() before printing again, even if reset
     printer.setDefault() # Restore printer to defaults
+
+def add_name(image, text):
+  draw = ImageDraw.Draw(image)
+  draw.text((30,30), 'Serra Angel', font=fnt)
 
 if __name__ == '__main__':
   main()
