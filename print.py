65413b3c4638c8f1b1cc92e8893c2ce2d0b109a5
marower <32189304+marower@users.noreply.github.com>
2020-08-24 17:12:33 -0700

    updated card formatting

--- print.py
+++ print.py
@@ -2,13 +2,13 @@
 # A local MariaDB instance is queried for a random card with the given CMC.
 # An image is constructed from a template, the card art, and card info and then printed.
 
-import mariadb
+import mysql.connector
 import os
 import sys
 import uuid
 from PIL import Image, ImageFont, ImageDraw
 
-fnt = ImageFont.truetype('VCR_OSD_MONO_1.001.ttf', 21)
+fnt = ImageFont.truetype('mplantin.ttf', 20)
 
 def main(argv):
     name, mana_cost, art_file, card_type, rules, artist, power, toughness = get_card(int(argv))[0]
@@ -26,12 +26,12 @@ def main(argv):
 
     print_file = str(uuid.uuid1()) + '.png'
 
-    # im.show()
-    im.save(print_file, 'PNG')
+    im.show()
+    #im.save(print_file, 'PNG')
 
     # lp is the CUPS print command
-    os.system('lp ' + print_file)
-    os.remove(print_file)
+    #os.system('lp ' + print_file)
+    #os.remove(print_file)
 
 
 def add_name(draw: ImageDraw, text: str):
@@ -39,7 +39,7 @@ def add_name(draw: ImageDraw, text: str):
 
 
 def add_mana_cost(draw: ImageDraw, text: str):
-    draw.text((300, 28), text, font=fnt)
+    draw.text((355 - fnt.getsize(text)[0], 28), text, font=fnt)
 
 # expects images sized 304x245
 def add_art(im_dest: Image, art: str):  
@@ -48,24 +48,47 @@ def add_art(im_dest: Image, art: str):
 
 
 def add_types(draw: ImageDraw, text: str):
-    draw.text((35, 295), text, font=fnt)
+    draw.text((30, 299), text, font=fnt)
 
-
+#max 286 width per line
 def add_rules(draw: ImageDraw, text: str):
-    draw.text((50, 320), text, font=fnt)
+    formatted_text = ''
+    for line in text.split('\n'):
+        new_line = ''
+        ret_line = ''
+        if fnt.getsize(text)[0] > 286: 
+            for word in line.split(' '):
+                if fnt.getsize(new_line  + word)[0]  > 286 or fnt.getsize(word)[0] > 286:
+                    if ret_line == '':  
+                        ret_line = new_line
+                    else:
+                        ret_line = ret_line + '\n' + new_line
+                    new_line = word + ' '
+                else:
+                    new_line = new_line + word + ' '
+            ret_line = ret_line + '\n' + new_line
+        if formatted_text == '':
+            formatted_text = ret_line
+        else:
+            formatted_text = formatted_text + '\n' + ret_line
+    
+    if len(formatted_text.split('\n')) > 7:
+        formatted_text = 'Rules text too long.\n'
 
+    draw.text((48, 322), formatted_text, font=fnt)
 
 def add_artist(draw: ImageDraw, text: str):
-    draw.text((35, 480), text, font=fnt)
+    draw.text((30, 480), text, font=fnt)
 
 
 def add_power_toughness(draw: ImageDraw, power: int, toughness: int):
-    draw.text((310, 480), str(power) + '/' + str(toughness), font=fnt)
+    text = str(power) + '/' + str(toughness)
+    draw.text((355 - fnt.getsize(text)[0], 480), text, font=fnt)
 
 
 def get_card(cmc):
-    conn = mariadb.connect(host='127.0.0.1', user='root',
-                           password='pass', db='mtg')
+    conn = mysql.connector.connect(host='127.0.0.1', user='root',
+                           password='pass', db='mtg', port=3307)
     cursor = conn.cursor()
     sql = (
         'select \
@@ -83,4 +106,4 @@ def get_card(cmc):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(2)
